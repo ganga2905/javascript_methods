dc26410fc04ae76f85d8a8a1a1f5250722766fe9
demo <gayathriganga04@gmail>
2026-02-17 23:30:35 +0530

    object create

---
+++ object.js
@@ -0,0 +1,34 @@
+/*Task 1 – Create and Access
+Create an object book with keys: title, author, and pages.
+ Print the author name using both dot and bracket notation
+*/
+
+let book={
+    title:"self-motivate",
+    author:"ganga",
+    pages:52
+}
+console.log(book.author);
+console.log(book["author"]);
+
+
+/*Task 2 – Add and Update
+Create an object user with properties name and age.
+ Add a new property email and update the age.
+*/
+
+
+
+let user={
+    name:"ganga",
+    age:21
+}
+user.email="gangagaythri04@gmail.com"
+user.age=22
+console.log(user);
+
+
+/*Task 3 – Loop Through
+Create an object student with 3 subjects and marks.
+ Loop through and print each subject with its mark.
+*/
