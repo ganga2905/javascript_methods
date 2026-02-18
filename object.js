1f609ccd1b39ae3ee7c82372e7f6196a8aef41c1
demo <gayathriganga04@gmail>
2026-02-18 14:14:14 +0530

    object task update

--- object.js
+++ object.js
@@ -32,3 +32,120 @@ console.log(user);
 Create an object student with 3 subjects and marks.
  Loop through and print each subject with its mark.
 */
+
+let student={
+    tamil:89,
+    english:87,
+    maths:66
+}
+
+for(let key in student){
+    console.log(key,student[key]);
+    
+}
+
+/*Task 4 – Method Example
+Create an object calculator with methods:
+●	add(a,b) → returns sum
+
+●	sub(a,b) → returns difference
+ Call both methods and print results.
+*/
+
+let calculator={
+    add:function(a,b) {
+        return a+b
+        
+        
+    },
+    sub:function(a,b) {
+        return a-b
+        
+    }
+}
+console.log(calculator.add(5,5));
+console.log(calculator.sub(10,5));
+
+/* Task 5 – Using this
+Create an object employee with properties name and salary.
+ Add a method details() that prints "Name: ___, Salary: ___" using this.
+*/
+
+let employee={
+    name:"ganga",
+    salary:30000,
+    details(){
+         return `Name: ${this.name}, Salary: ${this.salary}`;
+        
+    }
+}
+console.log(employee.details());
+
+/*
+Task 6 – Object.keys() / values()
+Create an object mobile and print all its keys and values using built-in methods.
+
+*/
+
+let mobile={
+    name:"samsung",
+    version:2.0,
+    price:80000
+}
+
+console.log(Object.keys(mobile));
+console.log(Object.values(mobile));
+
+
+/* Task 7 – Nested Object
+Create an object student with nested details:
+{
+  name: "Kavi",
+  marks: { maths: 80, science: 90 }
+}
+
+Access and print the science mark.
+*/
+let studentDetails={
+    name:"kavi",
+    marks:{maths:80, science:90}
+}
+console.log(studentDetails.marks.science);
+
+
+/*
+ Task 8 – Combine Objects
+Use the spread operator (...) to merge:
+const a = {name: "Ram"};
+const b = {age: 20};
+
+Result → {name: "Ram", age: 20}
+*/
+
+const a = {name: "Ram"};
+const b = {age: 20};
+
+const result={...a,...b};
+console.log(result);
+
+
+/* Task 9 – Delete Property
+Create an object car with properties brand, price, year.
+ Delete the year property and print the updated object.
+*/
+
+
+let car={
+    brand:"bmw",
+    price:5000000,
+    year:2035
+}
+console.log(car);
+
+delete car.year;
+console.log(car);
+
+
+
+
+
